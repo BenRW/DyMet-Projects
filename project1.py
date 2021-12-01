b752780fe6c8b1905422a4e1acc6fa4fb16f9a6d
Ben R-W <br2417@ic.ac.uk>
2021-12-01 20:34:35 +0100

    Added function to find max height and velocity and frequency

--- project1.py
+++ project1.py
@@ -36,17 +36,56 @@ def compute(t_tot, dt=0.1, w_0=0, theta_star=305, s=True):
 
     return t, z, w
 
+def find_characteristics(ts, zs, ws, printout=True):
+    w_max = np.amax(ws) # max vertical velocity of parcel
+    z_max = np.amax(zs) # max altitude of parcel
+    z_w_max = zs[np.argmax(ws)] # altitude where max velocity is reached
+
+    # finding the period
+    t_zero_crossing = ts[np.where(zs<1e-3)]
+    if len(t_zero_crossing)>1:
+        period = t_zero_crossing[1]-t_zero_crossing[0]
+        N = 2*np.pi/period
+    else:
+        period = "Could not be found"
+        N = "Could not be found"
+
+    if printout:
+        print("Max altitude: ", z_max, "m")
+        print("Max velocity: ", w_max, "m/s")
+        print("Max velocity reached at: ", z_w_max, "m altitude")
+        print("Period: ", period, "s")
+        print("Angular frequency: ", N, "s^{-1}")
+
+    return w_max, z_max, z_w_max, period, N
+
 t, z, w = compute(1000)
 t_, z_, w_ = compute(1000, s=False)
 
-plt.figure(1)
-plt.plot(t, z)
-plt.xlabel("Time [s]")
-plt.ylabel("Height [m]")
+w_max, z_max, z_w_max, period, N = find_characteristics(t, z, w)
+w_max, z_max, z_w_max, period, N = find_characteristics(t_, z_, w_)
 
-plt.figure(2)
-plt.plot(t_, z_)
-plt.xlabel("Time [s]")
-plt.ylabel("Height [m]")
+
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(211)
+ax2 = fig1.add_subplot(212)
+
+ax1.plot(t, z)
+ax1.set_ylabel("Height [m]")
+
+ax2.plot(t, w)
+ax2.set_xlabel("Time [s]")
+ax2.set_ylabel("Vertical Velocity [m/s]")
+
+fig2 = plt.figure()
+ax3 = fig2.add_subplot(211)
+ax4 = fig2.add_subplot(212)
+
+ax3.plot(t_, z_)
+ax3.set_ylabel("Height [m]")
+
+ax4.plot(t_, w_)
+ax4.set_xlabel("Time [s]")
+ax4.set_ylabel("Vertical Velocity [m/s]")
 
 plt.show()
